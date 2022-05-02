e3517ce1d3a25d3dfd1ef196d3cbbcb7fb6f93fa
EMINENCE <Emmyfedev@gmail.com>
2022-05-02 09:45:45 -0700

    adjusted img

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
  //selector
  var prev= document.querySelector('#prev');
  var next =document.querySelector('#next');
+ var productContainer= document.getElementById('productsContainer')
 
+ 
  next.addEventListener('click',  ()=>{
-     alert('working perfeectly')
+     productContainer.scrollLeft +=200;
  })
+
+ prev.addEventListener('click',  ()=>{
+    productContainer.scrollLeft -= 200;
+})
