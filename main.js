122b8482235ba6aa83d84d83b4b3f090eca5e131
EMINENCE <Emmyfedev@gmail.com>
2022-05-02 08:07:38 -0700

    mypage

---
+++ main.js
@@ -0,0 +1,7 @@
+ //selector
+ var prev= document.querySelector('#prev');
+ var next =document.querySelector('#next');
+
+ next.addEventListener('click',  ()=>{
+     alert('working perfeectly')
+ })
